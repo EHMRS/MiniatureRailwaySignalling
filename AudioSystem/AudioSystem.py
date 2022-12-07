32137bf8fee6ed4c579ad2d15cef1a611bfd38d3
Richard Franks <richard@ehmr.org.uk>
2022-12-07 12:00:07 +0000

    Added AudioSystem Python app

---
+++ AudioSystem/AudioSystem.py
@@ -0,0 +1,30 @@
+
+"""!
+@mainpage Signalling System Audio System
+@section intro_sec Introduction
+
+This is the audio code for the signalling system for the East Herts Miniature Railway. The
+project is written in python and handles all audio that is played through the station PA
+system
+
+@file main.py
+
+@brief Main application entry
+"""
+
+import logging
+from mqtt import mqtt
+from audio import audio
+
+logging.basicConfig(level=logging.DEBUG)
+
+def main():
+    """! Main entry point"""
+
+    logging.info("Booting")
+    mqtt.init()
+    audio.init()
+    while True:
+        audio.loop()
+
+main()
---
+++ AudioSystem/README.md
@@ -0,0 +1,4 @@
+# AudioSystem
+
+This software runs and listens to MQTT messages, playing audio as appropriate when
+requested to.
